8e2494334c526fdef9f70e045146a611a54396df
liyunhe <759943432@qq.com>
2017-03-27 11:21:20 +0800

    Initial Commit

---
+++ runtime理解/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  runtime理解
+//
+//  Created by mrlee on 2017/3/27.
+//  Copyright © 2017年 mrlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ runtime理解/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  runtime理解
+//
+//  Created by mrlee on 2017/3/27.
+//  Copyright © 2017年 mrlee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
