9159f3294edd65dbad6adc730339c724c48e7d33
liyunhe <759943432@qq.com>
2017-03-27 13:32:35 +0800

    first commit

---
+++ OC底层验证/OC底层验证/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  OC底层验证
+//
+//  Created by mrlee on 2017/3/27.
+//  Copyright © 2017年 mrlee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ OC底层验证/OC底层验证/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OC底层验证
+//
+//  Created by mrlee on 2017/3/27.
+//  Copyright © 2017年 mrlee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Person.h"
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        Person * p = [[Person alloc]init];
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
